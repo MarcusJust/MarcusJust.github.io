abf719523139ac9db447a9c35e5caf4e0e947acf
Marcus Just <filip.marcus.just@gmail.com>
2021-11-22 08:28:36 +0100

    js fix

--- scripts/script.js
+++ scripts/script.js
@@ -6,21 +6,21 @@ const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
 //counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+var counter = 1;
+const size = 500;
 
 carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
 
 //buttons listeners
 nextBtn.addEventListener("click",()=>{
-    if(counter >=carouselImages.length -1) return;
+    if(counter+1 >= carouselImages.length) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
 });
 
 prevBtn.addEventListener("click",()=>{
-    if(counter <=0)return;
+    if(counter+1 <= carouselImages.length )return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
